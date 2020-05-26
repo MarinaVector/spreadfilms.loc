c165eeaaebb8d09c3e2026e7e5538be371705dbd
Marinacvbn1811 <beluana1@mail.ru>
2020-05-26 17:50:58 +0500

    Form register and begin adapt

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,64 +6,58 @@
 
 
     <div class="row">
-        <div class="col-xs-10 col-md-8 col-lg-6 col-xl-4 offset-sm-1 mt-5 auth_page_block">
+        <div class="offset-lg-2 col-xl-3 col-xl-3 px-5 mt-md-5 col-12 form-bg">
             @include('layouts.notifications')
-            <div class="box-login">
-                <h2 class="text-center mb-3"><strong>SFM</strong>Registration</h2>
-                <p>Hello, nice that you want to register in our database. Please enter your name and email address here so that we can create your account. You will then receive a confirmation email.</p>
-                <hr>
-                <form method="POST" action="/register">
-                    {!! csrf_field() !!}
+            <form method="POST" action="/register">
+                {!! csrf_field() !!}
+
+                <div class="form-group mt-5 mt-lg-0">
+                    <h2 class="mb-3 text-center"><b>SFM</b>Registrieren</h2>
+                    <p class="mb-3">Hallo, schön, dass du dich in unserer Datenbank registrieren möchtest. Bitte trage
+                        hier zuerst deinen Namen und deine E-Mail Adresse ein damit wir deinen Account anlegen können.
+                        Anschließend erhältst du eine E-Mail zur Bestätigung.</p>
+                    <hr>
+
 
                     <div class="form-group">
-                        <label for="firstname">First name</label>
+                        <label for="exampleInputVorname">Vorname</label>
                         <input type="text" name="firstname" id="firstname" value="{{ old('firstname') }}"
-                        class="form-control">
+                               class="form-control">
                     </div>
 
                     <div class="form-group">
-                        <label for="surname">Surname</label>
+                        <label for="exampleInputNachname">Nachname</label>
                         <input type="text" name="surname" id="surname" value="{{ old('surname') }}"
-                        class="form-control">
+                               class="form-control">
                     </div>
 
-                    <div class="form-group">
-                        <label for="email">E-Mail</label>
-                        <input type="email" name="email" id="email" value="{{ old('email') }}" class="form-control">
-                    </div>
+                    <label for="exampleInputEmail1">E-Mai</label>
+                    <input type="email" name="email" id="email" value="{{ old('email') }}" class="form-control">
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone
+                        else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Passwort <small>(Mindestens 6 Zeichen)</small></label>
+                    <input type="password" name="password" id="password" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Passwort bestätigen</label>
+                    <input type="password" name="password_confirmation" id="password_confirmation" class="form-control">
+                </div>
+                <div class="form-group form-check">
+                    <input type="checkbox" class="custom-control-input" id="privacy_policy" name="privacy_policy">
+                    <label class="form-check-label" for="exampleCheck1">Ich bin mit der</label>
+                </div>
 
-                    <div class="form-group">
-                        <label for="password">Password</label>
-                        <small>(At least 6 characters)</small>
-                        <input type="password" name="password" id="password" class="form-control">
-                    </div>
+                <div class="form-group">
+                    <div class="g-recaptcha text-center" data-sitekey="{{ env('RECAPTCHAV2_SITEKEY') }}"></div>
+                </div>
 
-                    <div class="form-group">
-                        <label for="password_confirmation">Confirm Password</label>
-                        <input type="password" name="password_confirmation" id="password_confirmation"
-                        class="form-control">
-                    </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-form btn-primary btn-block">Registrieren</button>
+                </div>
 
-                    <div class="form-group">
-                        <div class="pretty p-default p-curve">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="privacy_policy"
-                                name="privacy_policy">
-                                <label class="custom-control-label" for="privacy_policy">I agree with the Privacy
-                                Policy.</label>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="g-recaptcha text-center" data-sitekey="{{ env('RECAPTCHAV2_SITEKEY') }}"></div>
-                    </div>
-
-                    <div class="form-group">
-                        <button type="submit" class="btn btn-primary btn-block">Register</button>
-                    </div>
-                </form>
-            </div>
+            </form>
         </div>
     </div>
 
