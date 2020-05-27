044b39b28ac559839fadbcdc0a2e0cbf5a98434f
eXCom <rain.tamper@gmail.com>
2020-05-27 14:26:56 +0300

    started profile page

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Auth;
 class ProfileController extends Controller
 {
     public function index() {
-        return 'profile page';
+        return view('profile.main');
     }
 }
---
+++ resources/views/profile/main.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.profile')
+
+@section('title', 'Profile')
+
+@section('content')
+    <div class="container">
+        Profile page
+    </div>
+@endsection
