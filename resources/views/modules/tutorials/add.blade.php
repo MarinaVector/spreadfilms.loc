eb7960e0eb1e6bb5a44005196e7b80244b194929
eXCom <rain.tamper@gmail.com>
2020-07-10 13:49:35 +0300

    removed unused code

--- resources/views/modules/tutorials/add.blade.php
+++ resources/views/modules/tutorials/add.blade.php
@@ -6,19 +6,6 @@
     <div class="container bg-add">
         @include('layouts.notifications')
 
-
-        <div>
-            <a href="/elfinder">Elfinder from vendor</a>
-        </div>
-
-        <br>
-        <br>
-        <br>
-        <br>
-        <label for="feature_image">Feature Image</label>
-        <input type="text" id="feature_image" name="feature_image" value="">
-        <a href="" class="popup_selector" data-inputid="feature_image">Select Image</a>
-
         <form class="no-bottom" method="POST" action="{{ route('module.tutorials.store') }}">
             @csrf
             <tutorial-page-builder></tutorial-page-builder>
