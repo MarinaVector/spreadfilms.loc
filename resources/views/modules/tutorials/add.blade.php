e2387c5d34f8e343572825b8d481a2782d6a452a
eXCom <rain.tamper@gmail.com>
2020-07-20 22:52:29 +0300

    ready to send tutorial for request (dynamic components are not ready)

--- resources/views/modules/tutorials/add.blade.php
+++ resources/views/modules/tutorials/add.blade.php
@@ -5,19 +5,14 @@
 @section('content')
     <div class="container bg-add">
         @include('layouts.notifications')
-
-
-
-        <form class="no-bottom" method="POST" action="{{ route('module.tutorials.store') }}">
-            @csrf
-            <input type="hidden" id="public_path" name="public_path" value="{{ '/userfiles/companies/' . Auth::user()->company()->id . '/public/' }}"/>
-            <input type="hidden" id="private_path" name="private_path" value="{{ '/userfiles/companies/' . Auth::user()->company()->id . '/private/' . Auth::user()->id . '/' }}"/>
-            <tutorial-page-builder
-                :usercompanycategories="'{{ $authUser->company()->companycategories->toJson() }}'"
-                :usercompanyroles="'{{ $authUser->company()->companyroles->toJson() }}'"
-                :usercompanyusers="'{{ $userCompanyUsers->toJson() }}'"
-            ></tutorial-page-builder>
-        </form>
+        <input type="hidden" id="public_path" name="public_path" value="{{ '/userfiles/companies/' . Auth::user()->company()->id . '/public/' }}"/>
+        <input type="hidden" id="private_path" name="private_path" value="{{ '/userfiles/companies/' . Auth::user()->company()->id . '/private/' . Auth::user()->id . '/' }}"/>
+        <tutorial-page-builder
+            :usercompanycategories="'{{ $authUser->company()->companycategories->toJson() }}'"
+            :usercompanyroles="'{{ $authUser->company()->companyroles->toJson() }}'"
+            :usercompanyusers="'{{ $userCompanyUsers->toJson() }}'"
+            :action="'{{ route('module.tutorials.store') }}'"
+        ></tutorial-page-builder>
     </div>
 @endsection
 
