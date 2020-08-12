e6b2c8c0e3c19ff9a8105aee328de5cb8b643309
eXCom <rain.tamper@gmail.com>
2020-08-12 13:32:18 +0300

    tutorials main page settings are now editable and being saved to database

--- resources/js/components/modules/tutorials/tutorials-view-page/ViewNestedMenu.vue
+++ resources/js/components/modules/tutorials/tutorials-view-page/ViewNestedMenu.vue
@@ -3,15 +3,23 @@
         <li class="" v-for="tutorial in tutorials">
             <div class="timeline-list pt-2">
                 <div class="timeline-badge up ml-3 mt-2">
-                    <i class="fas fa-minus fa-white fa-xs"></i></div>
+                    <i class="fas fa-minus fa-white fa-xs"></i>
+                </div>
 
                 <div class="timeline-panel">
                     <div class="timeline-body py-2">
                         {{ tutorial.label }}
                         <!-- <span class="number"></span> -->
+                        <span @click.prevent="toggleTutorial(index)"
+                              class="collapser-nested mr-1">
+                            <i
+                                class="fas fa-angle-up arr-down"
+                                :class="[`fa-angle-${tutorial.active ? 'down' : 'up'}`, `arr-${tutorial.active ? 'up' : 'down'}`]"
+                            />
+                        </span>
                     </div>
                 </div>
-                <view-nested-menu v-if="tutorial.children" :tutorials="tutorial.children"/>
+                <view-nested-menu v-if="tutorial.children" :tutorials="tutorial.children"></view-nested-menu>
             </div>
         </li>
     </ul>
@@ -25,17 +33,30 @@ export default {
 
     },
     created() {
-
+        console.log(this.$props.tutorials);
     },
     data() {
         return {
             tutorialsObj: {}
         }
     },
+    methods: {
+        toggleTutorial(index) {
+            this.tutorialsObj[index].active = !this.tutorialsObj[index].active
+        },
+    }
 }
 </script>
 
 <style>
+
+.collapser-nested {
+    position: absolute;
+    top: 40%;
+    transform: translateY(-50%);
+    right: 15px;
+    cursor: pointer;
+}
 
 .timeline-badge {
     width: 12px;
