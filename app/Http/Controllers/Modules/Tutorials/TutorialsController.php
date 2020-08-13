7cffd404943bbeed9a1e6806085ba841f4e7cbe7
beluana@mail.ru <vehksrf5577>
2020-08-14 02:23:18 +0500

    Blade view tutorial with image and text

--- app/Http/Controllers/Modules/Tutorials/TutorialsController.php
+++ app/Http/Controllers/Modules/Tutorials/TutorialsController.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Tutorial;
 use Illuminate\View\View;
+use App\Models\CompanyTutorialsSettings;
 
 class TutorialsController extends Controller
 {
@@ -350,15 +351,23 @@ class TutorialsController extends Controller
                 $userCompanyTutorialsNested[] = $resTutorial;
             }
         }
-        //dd($userCompanyTutorialsNested);
+        //dd($userCompanyTutorialsNested     'company_id',  $userCompany ->id);
         $userCompanyTutorialsNestedJSON = json_encode($userCompanyTutorialsNested);
-        $userCompanyTutorialsSettings = $userCompany->tutorialsSettings->toJSON();
+       // $userCompanyTutorialsSettings = $userCompany->tutorialsSettings->toJSON();
 
-        return view('modules.tutorials.view_tutorials')->with([
+        $settings =  CompanyTutorialsSettings::first();
+
+
+        return view('modules.tutorials.view_tutorials', compact('settings'))->with([
             'authUser' => $user,
             'tutorials' => $userCompanyTutorialsNestedJSON,
-            'settings' => $userCompanyTutorialsSettings]);
+            'settings' => $settings]);
     }
+
+
+
+
+
 
     final public function view($tutorial_id){
         dd($tutorial_id);
