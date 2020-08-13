d8cac7c30aed96be7b9097bd1b2bd2c639b84f34
beluana@mail.ru <vehksrf5577>
2020-08-13 15:02:11 +0500

    Btn and header

--- resources/views/modules/tutorials/view_tutorials.blade.php
+++ resources/views/modules/tutorials/view_tutorials.blade.php
@@ -20,8 +20,12 @@
                     </div>
                 </div>
             </div>
-            <div class="col-md-9">
 
+            <div class="col-md-8">
+                <div class="teaser-head p-3"><p>Start <br> Headline</p></div>
+                <button class="btn theme-background-horizontal teaser-button tutorial-link">
+                    Get start
+                </button>
             </div>
         </div>
     </div>
@@ -61,14 +65,47 @@
             border-radius: 3px;
             background-color: #000000e6;
             margin: 0 3px;
-            width: 3px !important;
+            width: 2px !important;
             position: absolute;
             min-height: 30px;
             height: 313px;
             top: 0px;
             display: block;
             max-height: 506px;
-            /* background-color: rgba(0,0,0,0.15); --*/
+        }
+
+        .teaser-button {
+            padding: 5px 10px;
+            display: inline-block !important;
+            cursor: pointer;
+            position: relative;
+            margin-top: 10px;
+            background: linear-gradient(40deg, #2096ff, #05ffa3) !important;
+            color: #fff;
+        }
+
+        .teaser-button:before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            background: #000;
+            opacity: 0;
+            transition: opacity .15s ease-in-out;
+            z-index: 2;
+        }
+
+       .teaser-head {
+            text-transform: uppercase;
+            text-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
+            font-size: 2.5em;
+            line-height: 1em;
+            color: #fff;
+           background:  #05ffa3;
+           opacity: 0.5;
+           display: inline-block;
         }
 
     </style>
