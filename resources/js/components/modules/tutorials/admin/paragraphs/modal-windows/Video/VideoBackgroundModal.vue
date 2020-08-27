9251a586f591c99f7e0698a9e5aab76fe6346231
beluana@mail.ru <vehksrf5577>
2020-08-28 03:18:36 +0500

    Frontend videobgmodal process no merge

--- resources/js/components/modules/tutorials/admin/paragraphs/modal-windows/Video/VideoBackgroundModal.vue
+++ resources/js/components/modules/tutorials/admin/paragraphs/modal-windows/Video/VideoBackgroundModal.vue
@@ -1,206 +1,158 @@
 <template>
-    <div class="modal fade container video-modal scroll-hidden" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="row scroll-show">
-            <div class="col-md-8">
-                <div class="edit-view">
-                    <div class="edit-inner">
-                        <div class="mb-3">
+    <div class="modal fade container-fluid" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="py-3 px-4">
+                <div class="scrollable modal-content">
+                    <div class="edit-content mb-2">
+                        <div class="modal-header p-1 ml-2">
+                            <label class="input-title"><i class="fas fa-heading mr-2"></i>Title</label>
+                        </div>
+                        <div class="modal-body">
+                            <ckeditor :editor="editor" v-model="NormalTextHeader" :config="editorConfig"></ckeditor>
+                        </div>
+                    </div>
 
-                            <div class="edit-content ">
-                                <div class="modal-header">
-                                    <label class="input-title"><i class="fas fa-heading mr-2"></i>Headline</label>
+                    <div class="part modal-content pt-2 pb-5 px-3">
+                        <div class="pull-right">
+                            <question-dropdown :answerdropdown="'The Video Banner is created automatically. ' +
+                         'Please not that no Banner can be created for a password-protected Video.'"></question-dropdown>
+                        </div>
+                        <label class="input-title"><i class="fas fa-image mr-2"></i>Background</label>
+                        <div class="elfinder-container"><input id="neu-1" type="hidden" data-type="bild" data-value="image" class="elfinder-idea">
+                            <button data-inputid="neu-1" data-dismiss="modal" class="popup_selector btn btn-default">Select image
+                            </button>
+                        </div>
+                        <hr>
+                        <label class="input-title"><i class="fas fa-film mr-2 ml-2"></i>Video</label>
+                        <div class="elfinder-container"><input type="text" placeholder="Video-URL" class="videourl">
+                        </div>
+                        <hr>
+                        <label class="input-title"><i data-fa-transform="rotate-45" class="fas fa-arrows-alt-h mr-2 ml-2"></i>Dimension</label>
+
+                        <div class="stay-logged-in ml-2">
+                            <div class="pretty p-default p-round">
+                                <div class="custom-checkbox pretty p-default p-round">
+                                    <label for="remember1">
+                                        <input type="radio" class="custom-control-input" name="remember" id="remember1">
+                                        <div class="roundchek mr-1"></div>
+                                        16:9
+                                    </label>
                                 </div>
-                                <div class="modal-body">
-                                    <ckeditor :editor="editor" v-model="NormalTextHeader" :config="editorConfig"></ckeditor>
-                                </div>
-                            </div>
-
-
-
-                            <div class="edit-content px-3">
-                                <label class="input-title"><i class="fas fa-image mr-2"></i>Image in the background</label>
-
-                                <div class="elfinder-container"><input id="neu-2" type="hidden" data-type="bild"
-                                                                       data-value="image" class="elfinder-idea">
-                                    <button data-inputid="neu-1" data-dismiss="modal" class="popup_selector btn btn-default">Select image
-                                    </button>
-                                </div>
-                                <hr>
-
-
-                                <label class="input-title"><i class="fas fa-image mr-2"></i>Banner</label>
-
-                                <div class="elfinder-container"><input id="neu-1" type="hidden" data-type="bild"
-                                                                       data-value="image" class="elfinder-idea">
-                                    <button data-inputid="neu-1" data-dismiss="modal" class="popup_selector btn btn-default">Select image
-                                    </button>
-                                </div>
-                                <hr>
-                                <label class="input-title"><i class="fas fa-film mr-2"></i>Video</label>
-                                <div class="elfinder-container"><input type="text" placeholder="Video-URL"
-                                                                       class="videourl">
-                                </div>
-                                <hr>
-                                <label class="input-title"><i data-fa-transform="rotate-45"
-                                                              class="fas fa-arrows-alt-h mr-2"></i>Dimension</label>
-
-                                <div class="stay-logged-in">
-                                    <div class="pretty p-default p-round">
-                                        <div class="custom-checkbox pretty p-default p-round">
-                                            <label for="remember1">
-                                                <input type="checkbox" class="custom-control-input" name="remember" id="remember1">
-                                                <div class="roundchek mr-1"></div>
-                                                16:9
-                                            </label>
-                                        </div>
-                                        <div class="pretty p-default p-round p-curve">
-                                            <div class="custom-checkbox">
-                                                <label for="remember2">
-                                                    <input type="checkbox" class="custom-control-input" name="remember" id="remember2">
-                                                    <div class="roundchek mr-1"></div>
-                                                    4:3
-                                                </label>
-                                            </div>
-                                        </div>
-
+                                <div class="pretty p-default p-round p-curve">
+                                    <div class="custom-checkbox">
+                                        <label for="remember2">
+                                            <input type="radio" class="custom-control-input" name="remember" id="remember2">
+                                            <div class="roundchek mr-1"></div>
+                                            4:3
+                                        </label>
                                     </div>
                                 </div>
 
-                                <hr>
-                                <label class="input-title"><i class="far fa-sticky-note mr-2"></i>Notice</label>
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="notes">
-                                            <table class="video-time">
-                                                <thead>
-                                                <tr>
-                                                    <th>Time</th>
-                                                    <th>Note</th>
-                                                </tr>
-                                                </thead>
-                                                <tbody class="list">
-                                                <tr>
-                                                    <td class="time">00:00</td>
-                                                    <td class="message"></td>
-                                                    <td class="remove"><span title="" class="edit-note-button"
-                                                                             data-original-title="Notiz bearbeiten"><i
-                                                        class="fas fa-pen mr-2"></i></span><span title=""
-                                                                                                 class="remove-note-button"
-                                                                                                 data-original-title=""><i
-                                                        class="fas fa-trash-alt"></i></span></td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="time">00:00</td>
-                                                    <td class="message"></td>
-                                                    <td class="remove"><span title="" class="edit-note-button"
-                                                                             data-original-title="Edit note"><i
-                                                        class="fas fa-pen mr-2"></i></span><span title=""
-                                                                                                 class="remove-note-button"
-                                                                                                 data-original-title="Remove note"><i
-                                                        class="fas fa-trash-alt"></i></span></td>
-                                                </tr>
-                                                <tr>
-                                                    <td class="time">00:00</td>
-                                                    <td class="message"></td>
-                                                    <td class="remove"><span title="" class="edit-note-button"
-                                                                             data-original-title="Edit note"><i
-                                                        class="fas fa-pen mr-2"></i></span><span title=""
-                                                                                                 class="remove-note-button"
-                                                                                                 data-original-title="Remove note"><i
-                                                        class="fas fa-trash-alt"></i></span></td>
-                                                </tr>
-                                                </tbody>
-                                                <tfoot>
-                                                <tr>
-                                                    <td><input type="hidden" value=""> <span
-                                                        class="time-picker"><input type="text" readonly="readonly"
-                                                                                   class="display-time"> <span
-                                                        class="clear-btn"
-                                                        style="">×</span>
-                                                        <!----> <div class="dropdown" style="display: none;"><div
-                                                            class="select-list"><ul class="hours"><li
-                                                            class="hint">HH</li> <li
-                                                            class="">00</li><li class="">01</li><li
-                                                            class="">02</li><li
-                                                            class="">03</li><li class="">04</li><li
-                                                            class="">05</li><li
-
-
-                                                            class="">56</li><li class="">57</li><li
-                                                            class="">58</li><li
-                                                            class="">59</li></ul>
-                                                            <!----></div></div></span></td>
-                                                    <td><input type="text" class="form-control"></td>
-                                                    <td><span title="" class="save-note-button"
-                                                              data-original-title="Save note"><i
-                                                        class="fas fa-save"></i></span></td>
-                                                </tr>
-                                                </tfoot>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
                             </div>
                         </div>
-                        <div class="buttons mt-5 video-btn">
-                            <div class="btn btn-primary save-btn"><span class="button-inner">
-            Save
-        </span></div>
-                            <div class="btn btn-secondary cancel-btn" data-dismiss="modal"><span class="button-inner">
-            Chancel
-        </span></div>
+
+                        <hr>
+                        <label class="input-title"><i class="far fa-sticky-note mr-2 ml-2"></i>Notice</label>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="notes">
+                                    <table class="video-time ml-2">
+                                        <thead>
+                                        <tr>
+                                            <th>Time</th>
+                                            <th>Note</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody class="list ml-2">
+                                        <tr>
+                                            <td class="time">00:00</td>
+                                            <td class="message"></td>
+                                            <td class="remove">
+                                                     <span title="" class="edit-note-button" data-original-title="Edit note">
+                                                         <i class="fas fa-pen mr-1"></i>
+                                                     </span>
+                                                <span title="" class="remove-note-button" data-original-title="">
+                                                         <i class="fas fa-trash-alt"></i>
+                                                     </span>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                        <tfoot>
+                                        <tr class="mt-2">
+                                            <td><input type="hidden" value="">
+                                                <vue-timepicker lazy format="mm:ss">
+                                                </vue-timepicker>
+                                            </td>
+                                            <td><input type="text" class="form-txt ml-5"></td>
+                                            <td><span title="" class="save-note-button ml-1" data-original-title="Save note">
+                                                <i class="fas fa-save"></i></span></td>
+                                        </tr>
+                                        </tfoot>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
 
+                <div class="modal-footer1 mt-3 ml-5">
+                    <button type="button" class="btn btn-primary btn-modal ml-5" data-dismiss="modal" @click="save()">
+                        Save changes
+                    </button>
+                    <button type="button" class="btn close-modal btn-modal" data-dismiss="modal" id="CancelModal">
+                        Close window
+                    </button>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import 'vue2-timepicker/dist/VueTimepicker.css';
+import VueTimepicker from 'vue2-timepicker/src/vue-timepicker.vue';
 
-    export default {
-        components: {
-
+export default {
+    name: "VideoSimple",
+    props: [
+        'header',
+        'body',
+    ],
+    components: {
+        VueTimepicker
+    },
+    data() {
+        return {
+            editor: ClassicEditor,
+            NormalTextHeader: this.$props.header ? this.$props.header : '<p></p>',
+            NormalTextBody: this.$props.body ? this.$props.body : '<p></p>',
+            editorConfig: {
+                // The configuration of the editor.
+            }
+        };
+    },
+    methods: {
+        save: function () {
+            this.$emit('saveData', this.NormalTextHeader, this.NormalTextBody)
         },
-        data() {
-            return {
-                editor: ClassicEditor,
-                NormalTextHeader: '<p></p>',
-                editorConfig: {
-                    // The configuration of the editor.
-                }
-            };
-        },
-        methods: {
-
-        },
-        created() {
-
-        },
-    };
-
+        cancel: function (event) {
+            if(event.target.id === 'NormalTextModal' || event.target.id === 'CancelModal') {
+                this.NormalTextHeader = this.$props.header;
+                this.NormalTextBody = this.$props.body;
+            }
+        }
+    },
+}
 </script>
 
-<style scoped>
+<style>
 
-    .scroll-hidden {
-        overflow: hidden;
-    }
+.scrollable {
+    overflow-y: scroll !important;
+    overflow-x: hidden;
+    height: 70vh;
+}
 
-    .scroll-show {
-        overflow-y: scroll;
-        overflow-x: hidden;
-        position: sticky;
-        margin: 0;
-    }
+</style>
 
-    .video-modal {
-        left: 30%;
-        height: 300px;
-    }
-
-
-    </style>
