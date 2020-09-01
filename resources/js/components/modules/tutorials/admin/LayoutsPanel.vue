d315eab8f2a5c56203b8bb9662441ad385ea5944
eXCom <rain.tamper@gmail.com>
2020-09-01 03:04:15 +0300

    small fix

--- resources/js/components/modules/tutorials/admin/LayoutsPanel.vue
+++ resources/js/components/modules/tutorials/admin/LayoutsPanel.vue
@@ -37,7 +37,7 @@
                 <button class="mr-2 btn-tutorial tooltip-btn btn-circle btn-layout p-0"
                         type="button" data-toggle="tooltip"
                         data-placement="bottom" title="Text and Image"
-                        @click="callParentAddParagraphBlock('TxtImg')">
+                        @click="callParentAddParagraphBlock('TextImg')">
                     <i class="fa fa-id-card fa-3x pt-2"></i>
                 </button>
             </div>
@@ -105,7 +105,7 @@
                 <button class="mr-2 btn-tutorial tooltip-btn btn-circle btn-layout p-0"
                         type="button" data-toggle="tooltip"
                         data-placement="bottom" title="Text and Image"
-                        @click="callParentAddParagraphBlock('TxtImg')">
+                        @click="callParentAddParagraphBlock('TextImg')">
                     <i class="fa fa-id-card fa-3x pt-2"></i>
                 </button>
 
