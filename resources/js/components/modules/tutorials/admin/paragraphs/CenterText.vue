c301b4cb83ff09303553773f659248ee35e576a2
beluana@mail.ru <vehksrf5577>
2020-09-17 02:04:01 +0500

    Front CenterText - text center

--- resources/js/components/modules/tutorials/admin/paragraphs/CenterText.vue
+++ resources/js/components/modules/tutorials/admin/paragraphs/CenterText.vue
@@ -20,11 +20,11 @@
                         </i>
                         <div class="mt-n1 mb-n1">Text</div>
                     </button>
-                    <div class="">
-                        <div v-bind:class=" {headertext:headerText} " class="text-header h2 text-left position-relative pb-0 normal_text" v-html="NormalTextHeader">
+                    <div class=" ">
+                        <div class="text-center text-header h2 position-relative pb-0 normal_text" v-html="NormalTextHeader">
                         </div>
 
-                        <div class="final-text text-justify" v-html="NormalTextBody"></div>
+                        <div class="position-relative text-center" v-html="NormalTextBody"></div>
                         <input type="hidden" name="normal_text_header" v-model="NormalTextHeader" class="normal_text_header">
                         <input type="hidden" name="normal_text_body" v-model="NormalTextBody" class="normal_text_body">
                     </div>
@@ -123,131 +123,6 @@
 </script>
 
 <style>
-
-    .inner-trigger {
-        transition: background-color .3s ease;
-        background-color: transparent;
-        border-radius: 2px;
-        padding: 5px 10px;
-        min-width: 100px;
-        text-align: center;
-    }
-
-    .text-button {
-        background-color: white;
-        border: none;
-    }
-
-    .tutorial-text,
-    .bgheadbefore {
-        width: 100%;
-        border: dotted 1px #333;
-    }
-
-    .tutorial-text:hover {
-        background-color: rgba(0, 0, 0, 0.5);
-        transition: 3s;
-    }
-
-    .tutorial-normal-text {
-        width: 100%;
-        transition: opacity .3s ease, background-color .5s ease;
-        background-color: white;
-    }
-
-    .tutorial-normal-text:hover {
-        background-color: rgba(0, 0, 0, 0.5);
-        transition: 3s;
-    }
-
-    .normalbefore {
-        border: dotted 1px #333;
-    }
-
-    .normalafter {
-        width: 100%;
-        height: auto;
-        border-bottom-width: 2px;
-        border-bottom-style: solid;
-        border-bottom-color: lightgray;
-    }
-
-    .btn-icon {
-        background: linear-gradient(to right, #3ecdf1, #2badce);
-        color: #fff;
-        border: none;
-        top: 20px;
-        width: 40px;
-        height: 40px;
-        border-radius: 5px;
-        text-align: center;
-        line-height: 40px;
-        cursor: pointer;
-        opacity: 0.5 !important;
-        transition: opacity .25s ease-in-out !important;
-        -moz-transition: opacity .25s ease-in-out !important;
-        -webkit-transition: opacity .25s ease-in-out !important;
-    }
-
-      .blueiconcolor {
-        color: #00bff9 !important;
-    }
-
-    .position-button {
-        z-index: 20;
-        position: relative;
-    }
-
-    .text-print {
-
-        column-count: 1;
-        margin-bottom: 20px;
-    }
-
-    .text-output {
-        position: absolute;
-    }
-
-    .headertext:before {
-        background: linear-gradient(40deg, #2096ff, #05ffa3) !important;
-        content: "";
-        width: 60px;
-        height: 4px;
-        background: #333;
-        top: -10px;
-        position: absolute;
-        left: 0;
-    }
-
-    #butTxt {
-        position: relative;
-        top: 0;
-        left: 0;
-    }
-
-    button {
-        cursor: pointer;
-    }
-
-    .btnafter {
-        display:none;
-        top: 45%;
-    }
-
-    .tutorial-normal-text:hover .btnafter{
-        display:block;
-    }
-
-
-    .btnbefore {
-        visibility: visible;
-    }
-
-    @media only screen and (max-width: 450px) {
-        #butTxt button {
-            left: 32%;
-        }
-    }
 
 
 
