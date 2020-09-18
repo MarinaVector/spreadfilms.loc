7c394d41ed49a83c10e86a7b850691c806c1221c
beluana@mail.ru <vehksrf5577>
2020-09-18 11:19:15 +0500

    Modal AddButton in comp TextWithBigFont (tutorials admin)

--- resources/js/components/modules/tutorials/admin/paragraphs/modal-windows/TextWithBigFont/ButtonModal.vue
+++ resources/js/components/modules/tutorials/admin/paragraphs/modal-windows/TextWithBigFont/ButtonModal.vue
@@ -1,27 +1,32 @@
 <template>
-    <div class="modal fade" id="ButtonModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    <div class="modal fade part" id="ButtonModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
          aria-hidden="true" @click="cancel">
         <div class="modal-dialog" role="document">
-            <div class="modal-content py-5">
-                <div class="modal-body">
-                    <div>
-                        <label class="input-title" for="text">Text</label>
-                        <input type="text" class="form-control" id="text">
+            <div class="py-3">
+                <div class="modal-body ">
+                    <div class="part edit-content">
+                        <div class="">
+                            <label class="input-title"><i class="fas fa-font mr-1"></i>Text</label>
+                            <input type="text" class="btnmodal-form form-control" id="text">
+                        </div>
+
+                        <div>
+                            <label class="input-title"><i class="fas fa-link mr-1"></i>Link</label>
+                            <input type="text" class="btnmodal-form form-control" id="link">
+                        </div>
                     </div>
 
-                    <div>
-                        <label class="input-title" for="link">Link</label>
-                        <input type="text" class="form-control" id="link">
-                    </div>
-
-                    <div>
+                    <div class="part edit-content">
+                         <div>
+                        <label class="input-title"><i class="fas fa-desktop mr-1"></i>Button Type</label>
+                        </div>
                         <p-radio name="dimension" v-model="color" :value="'coloured'">Coloured</p-radio>
                         <p-radio name="dimension" v-model="color" :value="'grey'">Grey</p-radio>
                     </div>
                 </div>
             </div>
 
-            <div class="modal-footer1 mt-3 ml-5">
+            <div class="modal-footer1 ml-5">
                 <button type="button" class="btn btn-primary btn-modal ml-5" data-dismiss="modal" @click="save()">
                     Save changes
                 </button>
@@ -68,3 +73,39 @@
         },
     };
 </script>
+
+<style>
+    .input-title {
+        margin-bottom: 10px;
+        display: inline-block;
+        border-bottom: solid 1px;
+        font-size: 0.8em;
+    }
+
+    .btnmodal-form, .btnmodal-form:focus {
+        font-size: 14px;
+        background: #fff;
+        box-shadow: 0 1px 1px 0 rgba(0,0,0,0.1);
+        color: #777777;
+        border: 1px solid #ced4da !important;
+        padding: 0 15px;
+        display: block;
+        width: 100%;
+        height: calc(1.6em + 0.75rem + 2px);
+        line-height: 1.5;
+        background-clip: padding-box;
+        border-radius: .25rem;
+        transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    }
+
+    .part {
+        padding: 15px;
+        margin-bottom: 15px;
+    }
+
+    .edit-content {
+        max-height: 80vh;
+        overflow: auto;
+    }
+
+</style>
