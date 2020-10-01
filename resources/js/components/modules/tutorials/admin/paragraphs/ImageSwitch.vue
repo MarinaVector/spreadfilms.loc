e9b8eae6a374dd9f874ee0a07fc3e36fb6d7ce2d
beluana@mail.ru <vehksrf5577>
2020-10-02 01:59:18 +0500

    Front components admin some progress no merge

--- resources/js/components/modules/tutorials/admin/paragraphs/ImageSwitch.vue
+++ resources/js/components/modules/tutorials/admin/paragraphs/ImageSwitch.vue
@@ -1,32 +1,44 @@
 <template>
-    <div class="container tutorial-video switch-grid content-txt" >
+    <div class="container content-txt" >
 
-        <div class="row my-2">
-            <div class="col-12">
+        <div class="row switch-grid my-2">
+            <div class="">
                 <button type="button" class="btn-icon draggable pull-left">
                     <i class="fa fa-arrows-v pt-2"></i>
                 </button>
 
-                <button type="button" class="btn-icon pull-right" @click="callParentDeleteParagraphBlock()">
-                    <i class="fa fa-trash-o pt-2"></i>
-                </button>
             </div>
 
-            <div class="col-12 my-md-5">
-                <div class="text-center my-md-5 position-relative">
-                    <button @click=showTextModal() class="text-button position-absolute btn-video py-3 px-5 mt-2"
-                            type="button">
+            <div class="">
+                <div class="col-lg-12 tutorial-text text-center py-5 pt-3" @click="">
+                    <button class="text-button py-2 my-5 px-5 pb-3" type="button">
                         <i class="fas fa-image blueiconcolor fa-2x">
                         </i>
-                        <p class="mt-n1 mb-n1">Switch</p>
+                        <p class="mb-n1">Image</p>
                     </button>
+                </div>
+
+                <div class="col-lg-12 py-5 tutorial-text content-txt position-relative text-center" @click="">
+                    <button class="text-button btnhitxt btnhip position-absolute py-2 px-5 my-2" type="button"                          >
+                        <i class="fas fa-bars blueiconcolor fa-2x">
+                        </i>
+                        <p class="mb-n1 mx-1">Text</p>
+                    </button>
+                    <div class="m-0 ml-5 position-relative" v-html=""></div>
                 </div>
             </div>
 
-            <div class="col-lg-12">
-                <button type="button" class="btn-icon pull-right" @click="callParentDuplicateParagraphBlock()">
-                    <i class="fa fa-files-o pt-2"></i>
-                </button>
+            <div class="switchbtn-grid">
+                <div class="">
+                   <button type="button" class="btn-icon pull-right" @click="callParentDeleteParagraphBlock()">
+                      <i class="fa fa-trash-o pt-2"></i>
+                   </button>
+                </div>
+                <div class="">
+                   <button type="button" class="btn-icon pull-right" @click="callParentDuplicateParagraphBlock()">
+                      <i class="fa fa-files-o pt-2"></i>
+                   </button>
+                </div>
             </div>
 
 
@@ -78,7 +90,17 @@ export default {
 
 .switch-grid {
     display: grid;
-    grid-gap: initial;
+    grid-template-columns: 1fr 3fr 1fr;
+}
+
+. switchbtn-grid {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr 1fr;
+    gap: 0px 0px;
+    grid-template-areas:
+    "."
+    ".";
 }
 
 </style>
