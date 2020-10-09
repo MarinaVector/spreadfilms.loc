11b6cca04678e84f8acfc1a4315f8ab4e69a7bba
beluana@mail.ru <vehksrf5577>
2020-10-10 00:36:20 +0500

    Front modal switch view page

--- resources/js/components/modules/tutorials/tutorials-view-page/paragraphs/ImageSwitch.vue
+++ resources/js/components/modules/tutorials/tutorials-view-page/paragraphs/ImageSwitch.vue
@@ -1,19 +1,20 @@
 <template>
-    <div class="container tutorial-switch content-txt">
-        <div class="row switch-grid my-2">
-            <div class="row">
-                <div class="col-lg-6 text-center d-inline-block py-5 pt-3 background-image"
-                     :style="{ backgroundImage: 'url(' + ImageBeforePath +')' }">
-                </div>
+    <div class="container">
+        <div class="row my-2">
+            <div class="col-12">
+                <div class="col-12">
+                    <div class="row">
+                         <div class="col-lg-6 text-center d-inline-block py-5 pt-3 background-image switch-img"
+                         :style="{ backgroundImage: 'url(' + ImageBeforePath +')' }">
+                          </div>
 
-                <div class="col-lg-6 text-center d-inline-block py-5 pt-3 background-image"
-                     :style="{ backgroundImage: 'url(' + ImageAfterPath + ')' }">
-                </div>
+                         <div class="col-lg-6 text-center d-inline-block py-5 pt-3 background-image switch-img"
+                         :style="{ backgroundImage: 'url(' + ImageAfterPath + ')' }">
+                         </div>
+                    </div>
 
-                <div class="col-lg-12 py-5 content-txt position-relative text-center">
-
-                    <div class="">
-                        <div class="text-header h2 text-left position-relative pb-0 normal_text"
+                    <div class="col-lg-12 py-5 content-txt position-relative text-center">
+                        <div class="text-header h2 text-left position-relative pb-0 normal_text headertext"
                              v-html="NormalTextHeader">
                         </div>
                         <div class="final-text text-justify" v-html="NormalTextBody"></div>
@@ -21,7 +22,6 @@
                 </div>
             </div>
         </div>
-
     </div>
 </template>
 
@@ -91,5 +91,8 @@ export default {
 
 <style>
 
+.switch-img {
+    min-height: 200px;
+}
 
 </style>
